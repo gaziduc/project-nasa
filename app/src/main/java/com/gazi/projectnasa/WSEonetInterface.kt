86ee91f43bc1f7a837240cf26ba1269c3ae3a637
KoHann <alexandre.vermeulen@epita.fr>
2021-06-21 12:06:23 +0200

    add pagination to eonet request

--- app/src/main/java/com/gazi/projectnasa/WSEonetInterface.kt
+++ app/src/main/java/com/gazi/projectnasa/WSEonetInterface.kt
@@ -2,9 +2,10 @@ package com.gazi.projectnasa
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface WSEonetInterface {
 
     @GET("events")
-    fun getAllEonetList() : Call<EonetObject>
+    fun getAllEonetList(@Query("limit") limit : Int = 25) : Call<EonetObject>
 }
